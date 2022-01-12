d8ab6d59f41c612ae36a9318a9007b33cb422975
woodimora <woodimora@gmail.com>
2022-01-12 15:11:56 +0900

    feat: 검색시 다시 목록으로 돌아가는 기능 추가

--- src/app.jsx
+++ src/app.jsx
@@ -1,44 +1,47 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
 import VideoDetails from './components/video_details/video_details';
 import VideoList from './components/video_list/video_list';
 
-function App({ youtube }) {
-  const [videos, setVideos] = useState([]);
-  const [selectedVideo, setSelectedVideo] = useState(null);
+function App({youtube}) {
+    const [videos, setVideos] = useState([]);
+    const [selectedVideo, setSelectedVideo] = useState(null);
 
-  const selectVideo = (video) => {
-    setSelectedVideo(video);
-  }
-  const search = (query) => {
-    setSelectedVideo(null);
-    youtube
-      .search(query) //
-      .then(videos => setVideos(videos));
-  }
+    const selectVideo = (video) => {
+        setSelectedVideo(video);
+    }
+    const search = (query) => {
+        setSelectedVideo(null);
 
-  useEffect(() => {
-    youtube
-      .mostPopular() //
-      .then(videos => setVideos(videos));
-  }, []);
+        youtube
+            .search(query) //
+            .then(videos => {
+                setVideos(videos);
+            });
+    }
 
-  return (
-    <div className={styles.app}>
-      <SearchHeader onSearch={search} />
-      <section className={styles.content}>
-        {selectedVideo &&
-          <div className={styles.details}>
-            <VideoDetails video={selectedVideo} />
-          </div>
-        }
-        <div className={styles.list}>
-          <VideoList videos={videos} onVideoClick={selectVideo} display={selectedVideo ? 'grid' : 'list'}/>
+    useEffect(() => {
+        youtube
+            .mostPopular() //
+            .then(videos => setVideos(videos));
+    }, []);
+
+    return (
+        <div className={styles.app}>
+            <SearchHeader onSearch={search}/>
+            <section className={styles.content}>
+                {selectedVideo &&
+                    <div className={styles.details}>
+                        <VideoDetails video={selectedVideo}/>
+                    </div>
+                }
+                <div className={styles.list}>
+                    <VideoList videos={videos} onVideoClick={selectVideo} display={selectedVideo ? 'grid' : 'list'}/>
+                </div>
+            </section>
         </div>
-      </section>
-    </div>
-  );
+    );
 }
 
 export default App;
