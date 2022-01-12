0f462c2d0c2f8e81b46ec769a40cac5b9bd7829c
woodimora <woodimora@gmail.com>
2022-01-12 20:26:13 +0900

    modify: DI형식으로 변경

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,16 +1,6 @@
-import axios from "axios";
-
 class Youtube {
-    constructor(key) {
-        this.youtube = axios.create({
-            baseURL: 'https://www.googleapis.com/youtube/v3',
-            params: {
-                key: key,
-                regionCode: 'KR',
-                part: 'snippet',
-                maxResults: 25,
-            },
-        });
+    constructor(httpClient) {
+        this.youtube = httpClient;
     }
 
     async mostPopular() {
